17ff232b02dfb0e1eb7b11279343c11f24f680c4
Pawel Peczkowski <peczkowskip@gmail.com>
2019-07-03 19:24:18 +0200

    PageToPdf module has been created

--- pageToPdf.js
+++ pageToPdf.js
@@ -1,19 +1,20 @@
 const puppeteer = require('puppeteer')
 const pathResolver = require('./utility/pathResolver')
 
-const pageScreenshot = async (website) => {
+const pageToPdf = async (website) => {
 	const time = Date.now()
 	const browser = await puppeteer.launch()
 	const page = await browser.newPage()
 	const options = {
-		path: `${pathResolver('screenshots/')}${time}_${website.match(/(?:[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9]))?\./g)}png`
+		path: `${pathResolver('pdf_files/')}${time}_${website.match(/(?:[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9]))?\./g)}pdf`,
+		format: 'A4'
 	}
-	await page.goto(website)
-	await page.screenshot(options)
+	await page.goto(website)	
+	await page.pdf(options)
 
 	await browser.close()
 	// eslint-disable-next-line no-console
 	console.log(`Time of executing task is: ${(Date.now() - time)/1000} sec.`)
 }
 
-module.exports = pageScreenshot
+module.exports = pageToPdf
