9771e5929bb9882f83a80503a933dadacf050b55
Pawel Peczkowski <peczkowskip@gmail.com>
2019-07-04 20:08:33 +0200

    Set resolution during screenshot task has been implemented

--- cli/index.js
+++ cli/index.js
@@ -1,6 +1,21 @@
 const prompts = require('prompts')
 const taskDefinitions = require('../tasks')
 
+const resolutions = {
+	'Desktop': {
+		width: 1920,
+		height: 1080
+	},
+	'Tablet': {
+		width: 800,
+		height: 640
+	},
+	'Mobile': {
+		width: 360,
+		height: 640
+	}
+}
+
 const main = async () => {
 
 	const response = await (async () => {
@@ -13,18 +28,25 @@ const main = async () => {
 				name: 'task'
 			},
 			{
+				type: prev => prev == 'page-screen' ? 'autocomplete' : null,
+				message: 'What resolution do you want to set?',
+				choices: Object.keys(resolutions).map(resolution =>({title: resolution})),
+				name: 'resolution'
+			},
+			{
 				type: 'text',
 				message: 'Which site you want to scrap?',
 				name: 'website'
-			}
+			},
 		])
 	})()
 
-	const {task, website} = response
+	const {resolution, task, website} = response
 	const taskDefinition = taskDefinitions[task]
+	const options = [website, resolutions[resolution]]
 
 	try {
-		await taskDefinition(website)
+		await taskDefinition(...options)
 	}
 	catch (error) {
 		console.error(error)
--- tasks/page-screen.js
+++ tasks/page-screen.js
@@ -1,13 +1,14 @@
 const puppeteer = require('puppeteer')
 const pathResolver = require('../utility/pathResolver')
 
-const pageScreenshot = async (website) => {
+const pageScreenshot = async (website, resolution) => {
 	const time = Date.now()
-	const browser = await puppeteer.launch()
+	const browser = await puppeteer.launch({headless: true})
 	const page = await browser.newPage()
 	const options = {
 		path: `${pathResolver('screenshots')}${time}_${website.match(/(?:[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9]))?\./g)}png`
 	}
+	await page.setViewport(Object.assign({deviceScaleFactor: 1}, resolution))
 	await page.goto(website)
 	await page.screenshot(options)
 
